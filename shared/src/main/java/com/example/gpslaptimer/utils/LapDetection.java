7b92dccadb43c150427d1a4f4270e2c35780d30e
dyellow3 <116983619+dyellow3@users.noreply.github.com>
2026-02-05 17:39:22 -0500

    Update LapDetection.java
    Reduced nesting

--- shared/src/main/java/com/example/gpslaptimer/utils/LapDetection.java
+++ shared/src/main/java/com/example/gpslaptimer/utils/LapDetection.java
@@ -24,65 +24,65 @@ public class LapDetection {
         double fastestTime = Double.POSITIVE_INFINITY;
         Lap fastestLap = null;
 
-        if (finishLine != null) {
-            LatLng finishLineStartPoint = finishLine.get(0);
-            LatLng finishLineEndPoint = finishLine.get(1);
-
-            // Now that we have finish line, we can find when it is crossed, and mark that as a lap
-            List<Lap> laps = new ArrayList<>();
-            int startIndex = 0;
-            double carryOverTime = 0;
-
-            // Loop through each point, looking at if the line between previous and current point intersects finish
-            for (int i = 1; i < locations.size(); i++) {
-                LatLng prevPoint = new LatLng(locations.get(i - 1).getLatitude(), locations.get(i - 1).getLongitude());
-                LatLng currPoint = new LatLng(locations.get(i).getLatitude(), locations.get(i).getLongitude());
-                LatLng intersectionPoint = getLineSegmentIntersection(prevPoint, currPoint, finishLineStartPoint, finishLineEndPoint);
-
-                // Intersection detected
-                if (intersectionPoint != null && i - startIndex > 2) {
-                    List<Location> lap = new ArrayList<>(locations.subList(startIndex, i + 1));
-                    Pair<Double, Double> lapTime = getLapTime(lap, finishLineStartPoint, finishLineEndPoint);
-                    if (startIndex != 0) {
-                        Lap currLap = new Lap(lap, lapTime.first + carryOverTime, laps.size());
-                        if ((lapTime.first + carryOverTime) < fastestTime) {
-                            fastestLap = currLap;
-                            fastestTime = lapTime.first + carryOverTime;
-                        }
-                        laps.add(currLap);
-                    } else {
-                        laps.add(new Lap(lap, 0.0, laps.size()));
-                    }
-                    carryOverTime = lapTime.second;
-                    startIndex = i;
-                }
-            }
-
-            // Add leftover lap data
-            if (startIndex < locations.size() - 1) {
-                List<Location> finalLap = new ArrayList<>(locations.subList(startIndex, locations.size()));
-                double finalLapTime = 0;
-                for (int i = 0; i < finalLap.size(); i++) {
-                    finalLapTime += 1;
-                }
-                laps.add(new Lap(finalLap, finalLapTime + carryOverTime, laps.size()));
-            }
-
-            // Add variance from fastest to each lap
-            for (int i = 1; i < laps.size() - 1; i++) {
-                Lap curr = laps.get(i);
-                curr.setLapVariation(curr.getLapTime() - fastestTime);
-            }
-
-            if (laps.isEmpty()) {
-                Log.d(TAG, "Could not detect any laps");
-                return new LapDetectionResult(new ArrayList<>(), null, null);
-            }
-            return new LapDetectionResult(laps, fastestLap, finishLine);
-        } else {
+        if (finishLine == null) {
             Log.d(TAG, "Could not detect start / finish points");
             return new LapDetectionResult(new ArrayList<>(), null, null);
         }
+
+        LatLng finishLineStartPoint = finishLine.get(0);
+        LatLng finishLineEndPoint = finishLine.get(1);
+
+        // Now that we have finish line, we can find when it is crossed, and mark that as a lap
+        List<Lap> laps = new ArrayList<>();
+        int startIndex = 0;
+        double carryOverTime = 0;
+
+        // Loop through each point, looking at if the line between previous and current point intersects finish
+        for (int i = 1; i < locations.size(); i++) {
+            LatLng prevPoint = new LatLng(locations.get(i - 1).getLatitude(), locations.get(i - 1).getLongitude());
+            LatLng currPoint = new LatLng(locations.get(i).getLatitude(), locations.get(i).getLongitude());
+            LatLng intersectionPoint = getLineSegmentIntersection(prevPoint, currPoint, finishLineStartPoint, finishLineEndPoint);
+
+            // Intersection detected
+            if (intersectionPoint != null && i - startIndex > 2) {
+                List<Location> lap = new ArrayList<>(locations.subList(startIndex, i + 1));
+                Pair<Double, Double> lapTime = getLapTime(lap, finishLineStartPoint, finishLineEndPoint);
+                if (startIndex != 0) {
+                    Lap currLap = new Lap(lap, lapTime.first + carryOverTime, laps.size());
+                    if ((lapTime.first + carryOverTime) < fastestTime) {
+                        fastestLap = currLap;
+                        fastestTime = lapTime.first + carryOverTime;
+                    }
+                    laps.add(currLap);
+                } else {
+                    laps.add(new Lap(lap, 0.0, laps.size()));
+                }
+                carryOverTime = lapTime.second;
+                startIndex = i;
+            }
+        }
+
+        // Add leftover lap data
+        if (startIndex < locations.size() - 1) {
+            List<Location> finalLap = new ArrayList<>(locations.subList(startIndex, locations.size()));
+            double finalLapTime = 0;
+            for (int i = 0; i < finalLap.size(); i++) {
+                finalLapTime += 1;
+            }
+            laps.add(new Lap(finalLap, finalLapTime + carryOverTime, laps.size()));
+        }
+
+        // Add variance from fastest to each lap
+        for (int i = 1; i < laps.size() - 1; i++) {
+            Lap curr = laps.get(i);
+            curr.setLapVariation(curr.getLapTime() - fastestTime);
+        }
+
+        if (laps.isEmpty()) {
+            Log.d(TAG, "Could not detect any laps");
+            return new LapDetectionResult(new ArrayList<>(), null, null);
+        }
+        return new LapDetectionResult(laps, fastestLap, finishLine);
     }
 
 
@@ -93,32 +93,36 @@ public class LapDetection {
 
         Grid grid = Grid.createGrid(locations, gridBounds, GRID_SIZE, DIRECTION_TOLERANCE);
 
-        if (grid != null) {
-            GridCell maxCountCell = grid.getMaxCountCell();
-            if (maxCountCell != null && maxCountCell.getCount() > 1) {
-                LatLng centerLatLng = getAveragePoint(maxCountCell.getPoints());
-                LatLng directionVector = maxCountCell.getDirectionVector();
-
-                // Perpendicular vector
-                double perpendicularLat = -directionVector.longitude;
-                double perpendicularLng = directionVector.latitude;
-                double distanceInDegrees = LINE_LENGTH / 111111; // Approximate conversion factor: 1 degree ≈ 111,111 meters
-
-                // End points of the perpendicular line
-                double startLat = centerLatLng.latitude - (perpendicularLat * distanceInDegrees / 2);
-                double startLng = centerLatLng.longitude - (perpendicularLng * distanceInDegrees / 2);
-                double endLat = centerLatLng.latitude + (perpendicularLat * distanceInDegrees / 2);
-                double endLng = centerLatLng.longitude + (perpendicularLng * distanceInDegrees / 2);
-
-                List<LatLng> finishLine = new ArrayList<>();
-                finishLine.add(new LatLng(startLat, startLng));
-                finishLine.add(new LatLng(endLat, endLng));
-
-                return finishLine;
-            }
+        if (grid == null) {
+            return null;
         }
 
-        return null;
+        GridCell maxCountCell = grid.getMaxCountCell();
+
+        if (maxCountCell == null || maxCountCell.getCount() <= 1) {
+            return null;
+        }
+        
+        LatLng centerLatLng = getAveragePoint(maxCountCell.getPoints());
+        LatLng directionVector = maxCountCell.getDirectionVector();
+
+        // Perpendicular vector
+        double perpendicularLat = -directionVector.longitude;
+        double perpendicularLng = directionVector.latitude;
+        double distanceInDegrees = LINE_LENGTH / 111111; // Approximate conversion factor: 1 degree ≈ 111,111 meters
+
+        // End points of the perpendicular line
+        double startLat = centerLatLng.latitude - (perpendicularLat * distanceInDegrees / 2);
+        double startLng = centerLatLng.longitude - (perpendicularLng * distanceInDegrees / 2);
+        double endLat = centerLatLng.latitude + (perpendicularLat * distanceInDegrees / 2);
+        double endLng = centerLatLng.longitude + (perpendicularLng * distanceInDegrees / 2);
+
+        List<LatLng> finishLine = new ArrayList<>();
+        finishLine.add(new LatLng(startLat, startLng));
+        finishLine.add(new LatLng(endLat, endLng));
+
+        return finishLine;
+        
     }
 
     public static Pair<Double, Double> getLapTime(List<Location> lap, LatLng finishLineStartPoint, LatLng finishLineEndPoint) {
@@ -179,17 +183,17 @@ public class LapDetection {
 
         if (determinant == 0) {
             return null;
+        }
+        
+        double longitude = (b2 * c1 - b1 * c2) / determinant;
+        double latitude = (a1 * c2 - a2 * c1) / determinant;
+
+        LatLng intersection = new LatLng(latitude, longitude);
+
+        if (onSegment(p1, intersection, q1) && onSegment(p2, intersection, q2)) {
+            return intersection;
         } else {
-            double longitude = (b2 * c1 - b1 * c2) / determinant;
-            double latitude = (a1 * c2 - a2 * c1) / determinant;
-
-            LatLng intersection = new LatLng(latitude, longitude);
-
-            if (onSegment(p1, intersection, q1) && onSegment(p2, intersection, q2)) {
-                return intersection;
-            } else {
-                return null;
-            }
+            return null;
         }
     }
 
